09d0f5976030748b0132aa529a27531fe9b8f2e2
Prowpdev <jhomignacio.ifliba@gmail.com>
2025-11-26 21:46:49 +0800

    update table data

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,5 @@
 import 'tabulator-tables/dist/css/tabulator.min.css';
+import '../scss/main.scss';
+// import '../scss/tabulator/dark.scss';
+import '../scss/tabulator/light.scss';
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './assets/js/index.js', // your main JS file
+    entry: './assets/js/index.js',
+
     output: {
         path: path.resolve(__dirname, 'static/dist'),
         filename: 'bundle.js',
     },
+
     module: {
         rules: [
             {
@@ -13,11 +16,33 @@ module.exports = {
                 exclude: /node_modules/,
                 use: { loader: 'babel-loader' },
             },
+
+            // CSS Loader
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ],
+            },
+
+            // SCSS Loader
+            {
+                test: /\.scss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader, // extract CSS to file
+                    'css-loader',                // turn css into JS modules
+                    'sass-loader'                // compile SCSS → CSS
+                ],
             },
         ],
     },
-    mode: 'development', // switch to 'production' when deploying
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles.css', // output css file
+        }),
+    ],
+
+    mode: 'development',
 };
